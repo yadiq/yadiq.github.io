4579f397a83d73a2e8ffe88d8a1e9a939b6eadfb
gyd <yadiq@qq.com>
2024-11-29 18:31:08 +0800

    修改主题

--- themes/hexo-theme-yuzu/pug-helpers/menu/itemWidget.js
+++
@@ -1,9 +0,0 @@
-exports.default = function itemWidget(item) {
-  const widgetName = item.widget.toLowerCase();
-
-  return this.partial('_widget/' + widgetName, {
-    category: item.name,
-    cat_link: item.link,
-    showNum: item.show_num || 3,
-  });
-}
